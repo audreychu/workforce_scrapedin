509f6c2e96919ab311a63b98f6701f9aa742a2c4
Audrey Chu <audreychu27@gmail.com>
2019-07-01 16:45:04 -0400

    Iterated git add scrapedin_mult_selenium_v2.py ! Next steps, use list from NSC

--- scrapedin_mult_selenium_v2.py
+++ scrapedin_mult_selenium_v2.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from parsel import Selector
+from pandas import ExcelWriter
 import pandas as pd
 import numpy as np
 
@@ -30,77 +31,65 @@ majors_ = ['"Data Science"', '"Statistics"', '"Computer Science"']
 criteria = ['site:linkedin.com/in/ AND '+a+' AND '+b+' AND '+c for a,b,c in zip(names_, schools_, majors_)]
 
 
-app = pd.DataFrame()
 
-for i in criteria:
-    d = pd.DataFrame([i])
-    app = app.append(d)
+final = pd.DataFrame()
 
-
-
-
-# Search for people by name, school, and major
-search_query = driver.find_element_by_name('q')
-# search_query.send_keys('site:linkedin.com/in/ AND "Rebecca Lindner" \
-#                       AND "Columbia" AND "Data Science"')
-search_query.send_keys(criteria[1])
-search_query.send_keys(Keys.RETURN)
-
-# Take the first URL from google search -- will need to expand later
-linkedin_url = driver.find_elements_by_class_name('iUh30')[0].text
-
-# Return LI page
-driver.get(linkedin_url)
-sel = Selector(text = driver.page_source)
-
-# Variables of interest
-name = sel.xpath('//*[starts-with(@class, "topcard__name")]/text()').extract_first()
-job_nogrowth = sel.xpath('//*[starts-with(@class, "section-item__title position__title")]/text()').getall()
-job_growth = sel.xpath('//*[starts-with(@class, "section-item__title experience-group-item__title")]/text()').getall()
-co_nogrowth = sel.xpath('//*[starts-with(@class, "position__company-name")]/text()').getall()
-co_growth = sel.xpath('//*[starts-with(@class, "section-item__title experience-group-header__title")]/text()').getall()
-schools = sel.xpath('//*[starts-with(@class, "section-item__title education-item__title")]/text()').getall()
-degrees = sel.xpath('//*[starts-with(@class, "education-item__degree-info")]/text()').getall()
-location = sel.xpath('//*[starts-with(@class, "topcard__location")]/text()').extract_first()
-industry = sel.xpath('//*[starts-with(@class, "topcard__industry")]/text()').extract_first()
-
-# time ranges include education. No difference between g/ng
-# How do we know which job category starts/matches with time ranges?
-# 4 #### marks education. Anything after includes volunteering, certifs, etc.
-t_start = sel.xpath('//*[starts-with(@class, "date-range__start-date")]/text()').getall()
-t_end = sel.xpath('//*[starts-with(@class, "date-range__end-date")]/text()').getall()
-
-
-
-# Create dataframe
-data = [[name, schools, degrees, location, industry, job_nogrowth, job_growth, co_nogrowth, co_growth, t_start, t_end]]
-df = pd.DataFrame(data, columns = ['name'
-                                   , 'schools'
-                                   , 'degrees'
-                                   , 'location'
-                                   , 'industry'
-                                   , 'job_nogrowth'
-                                   , 'job_growth'
-                                   , 'co_nogrowth'
-                                   , 'co_growth'
-                                   , 't_start'
-                                   , 't_end'])
-df = df.append(df)
-
-driver.quit()
+for i in criteria:    
+    driver = webdriver.Chrome('/Users/Audrey Chu/Desktop/chromedriver')
+    driver.get('https://www.google.com')
+    
+    # Search for people by name, school, and major
+    search_query = driver.find_element_by_name('q')
+    # search_query.send_keys('site:linkedin.com/in/ AND "Rebecca Lindner" \
+    #                       AND "Columbia" AND "Data Science"')
+    search_query.send_keys(i)
+    search_query.send_keys(Keys.RETURN)
+    
+    # Take the first URL from google search -- will need to expand later
+    linkedin_url = driver.find_elements_by_class_name('iUh30')[0].text
+    
+    # Return LI page
+    driver.get(linkedin_url)
+    sel = Selector(text = driver.page_source)
+    
+    # Variables of interest
+    name = sel.xpath('//*[starts-with(@class, "topcard__name")]/text()').extract_first()
+    job_nogrowth = sel.xpath('//*[starts-with(@class, "section-item__title position__title")]/text()').getall()
+    job_growth = sel.xpath('//*[starts-with(@class, "section-item__title experience-group-item__title")]/text()').getall()
+    co_nogrowth = sel.xpath('//*[starts-with(@class, "position__company-name")]/text()').getall()
+    co_growth = sel.xpath('//*[starts-with(@class, "section-item__title experience-group-header__title")]/text()').getall()
+    schools = sel.xpath('//*[starts-with(@class, "section-item__title education-item__title")]/text()').getall()
+    degrees = sel.xpath('//*[starts-with(@class, "education-item__degree-info")]/text()').getall()
+    location = sel.xpath('//*[starts-with(@class, "topcard__location")]/text()').extract_first()
+    industry = sel.xpath('//*[starts-with(@class, "topcard__industry")]/text()').extract_first()
+    
+    # time ranges include education. No difference between g/ng
+    # How do we know which job category starts/matches with time ranges?
+    # 4 #### marks education. Anything after includes volunteering, certifs, etc.
+    t_start = sel.xpath('//*[starts-with(@class, "date-range__start-date")]/text()').getall()
+    t_end = sel.xpath('//*[starts-with(@class, "date-range__end-date")]/text()').getall()
+    
+    
+    # Create dataframe
+    data = [[name, schools, degrees, location, industry, job_nogrowth, job_growth, co_nogrowth, co_growth, t_start, t_end]]
+    final = final.append(data)
+    
+    driver.quit()
 
 
 
-# How do we return an entire section with the tag 'div'
-# sec = sel.xpath('//*[starts-with(@class, "section-item__content education-item__content")]/text()').getall()
 
-# Do we want description? Differs between g/ng
-# des_nogrowth = sel.xpath('//*[starts-with(@class, "position-body__description")]/text()').getall()
-
-# MS, Data Science (Computational Statistics)
-# BA, Applied Statistics
-# BS, Managerial Economics
-
-
-# Terminates the application
-driver.quit()
+# Format final data frame
+final.columns = ['Name'
+                 , 'Schools'
+                 , 'Degrees'
+                 , 'Location'
+                 , 'Industry'
+                 , 'Job_NoGrowth'
+                 , 'Job_Growth'
+                 , 'Co_NoGrowth'
+                 , 'Co_Growth'
+                 , 'T_Start'
+                 , 'T_End']
+final = final.reset_index(drop = True)
+final.to_excel(r"C:\Users\Audrey Chu\Desktop\workforce_scrapedin\ThreeTest.xlsx")
